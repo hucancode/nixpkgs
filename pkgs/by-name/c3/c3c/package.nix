42540e5e9b8f4cb0164a05754d3e46ae62b68b4b
Bang Nguyen Huu <hucancode@gmail.com>
2025-03-04 13:26:58 +0900

    update c3c to 0.6.8

--- pkgs/by-name/c3/c3c/package.nix
+++ pkgs/by-name/c3/c3c/package.nix
@@ -1,37 +1,55 @@
 {
-  llvmPackages,
   lib,
   fetchFromGitHub,
+  llvmPackages,
   cmake,
   python3,
   curl,
   libxml2,
   libffi,
   xar,
-  versionCheckHook,
-}:
+  rev ? "unknown",
+  debug ? false,
+  checks ? false,
+}: let 
+  inherit (builtins) readFile elemAt;
+  # inherit (lib.sources) cleanSourceWith cleanSource; 
+  inherit (lib.lists) findFirst;
+  inherit (lib.asserts) assertMsg;
+  inherit (lib.strings) hasInfix splitString removeSuffix removePrefix optionalString;
+in llvmPackages.stdenv.mkDerivation (finalAttrs: {
 
-llvmPackages.stdenv.mkDerivation (finalAttrs: {
   pname = "c3c";
-  version = "0.6.6";
+  version = "0.6.8";
 
   src = fetchFromGitHub {
     owner = "c3lang";
     repo = "c3c";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-+rNj1SmiBYBw3Ncx2uS8X5OA/qDvJ8SFpZOQVRCgvmM=";
+    hash = "sha256-CG/fG/QVuL5tKTK9xFXrwsC0riCJilE8BE+v+tmoTVI=";
   };
-
+  
+  cmakeBuildType = if debug then "Debug" else "Release";
+  
   postPatch = ''
-    substituteInPlace CMakeLists.txt \
-      --replace-fail "\''${LLVM_LIBRARY_DIRS}" "${llvmPackages.lld.lib}/lib ${llvmPackages.llvm.lib}/lib"
+    substituteInPlace git_hash.cmake \
+      --replace-fail "\''${GIT_HASH}" "${rev}"
   '';
 
-  nativeBuildInputs = [ cmake ];
+  cmakeFlags = [
+    "-DC3_ENABLE_CLANGD_LSP=${if debug then "ON" else "OFF"}"
+    "-DC3_LLD_DIR=${llvmPackages.lld.lib}/lib"
+    "-DLLVM_CRT_LIBRARY_DIR=${llvmPackages.compiler-rt}/lib/darwin"
+  ];
+
+  nativeBuildInputs = [ 
+    cmake 
+    llvmPackages.llvm
+    llvmPackages.lld 
+    llvmPackages.compiler-rt
+  ];
 
   buildInputs = [
-    llvmPackages.llvm
-    llvmPackages.lld
     curl
     libxml2
     libffi
@@ -39,24 +57,24 @@ llvmPackages.stdenv.mkDerivation (finalAttrs: {
 
   nativeCheckInputs = [ python3 ];
 
-  doCheck = llvmPackages.stdenv.system == "x86_64-linux";
+  doCheck = llvmPackages.stdenv.system == "x86_64-linux" && checks;
 
   checkPhase = ''
     runHook preCheck
-    ( cd ../resources/testproject; ../../build/c3c build )
-    ( cd ../test; python src/tester.py ../build/c3c test_suite )
+    ( cd ../resources/testproject; ../../build/c3c build --trust=full )
+    ( cd ../test; ../build/c3c compile-run -O1 src/test_suite_runner.c3 -- ../build/c3c test_suite )
     runHook postCheck
   '';
 
-  nativeInstallCheckInputs = [ versionCheckHook ];
-  doInstallCheck = true;
 
   meta = with lib; {
     description = "Compiler for the C3 language";
     homepage = "https://github.com/c3lang/c3c";
     license = licenses.lgpl3Only;
     maintainers = with maintainers; [
+      luc65r
       anas
+      vssukharev
     ];
     platforms = platforms.all;
     mainProgram = "c3c";
